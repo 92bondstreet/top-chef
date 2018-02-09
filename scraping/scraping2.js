4358586e0b05ed2a625b0eb3645b1789c128cddd
Gregoire Albizzati <gregalbizz@gmail.com>
2018-02-09 01:39:26 +0100

    Refactoring and reworking of the function getUrlsCitiesFromLinks
    getUrlsCitiesFromLinks is now loadUrlsCitiesFromLinks
     it's an asynchroneousfunction which takes an url to find all the links redirecting to a city page where there is all the restaurants in the concerned city.
    This function doesn't return any thing. However, because it's asynchroneous it acts like a promise (eventhough it doesn't actually return something, for further information look at line 49)

--- scraping/scraping2.js
+++ scraping/scraping2.js
@@ -1,33 +1,56 @@
-import cheerio from 'cheerio';
-import fetch from 'node-fetch';
+const cheerio = require('cheerio');
+const fetch = require('node-fetch');
 
 // factory function for the Cities Handler
 const Cities = () => {
-  let IsReady = false;
-  const Links = ["https://restaurant.michelin.fr/restaurants-etoiles-france/page-1",
-    "https://restaurant.michelin.fr/restaurants-etoiles-france/page-2",
-    "https://restaurant.michelin.fr/restaurants-etoiles-france/page-3",
-    "https://restaurant.michelin.fr/restaurants-etoiles-france/page-4",
-    "https://restaurant.michelin.fr/restaurants-etoiles-france/page-5"];
+
   const UrlCities = [];
 
-  //get all the links for all the cities
-  const getUrlsCitiesFromLinks = async url =>{
+  const Links = ["https://restaurant.michelin.fr/restaurants-etoiles-france/",
+    "https://restaurant.michelin.fr/restaurants-etoiles-france/page-2/",
+    "https://restaurant.michelin.fr/restaurants-etoiles-france/page-3/",
+    "https://restaurant.michelin.fr/restaurants-etoiles-france/page-4/"];
 
+  //get all the links for all the cities
+  //this function return a Promise so once it's been called on all the links
+  //and it's been resolved, you can safely access to UrlCities
+  const loadUrlsCitiesFromLinks = async url =>{
     try {
       const response = await fetch(url);
-      const loadedHtml = await cheerio.load(response);
-      //parse the loadedHtml
-      return //the stuff and the promise resolved
+      const html = await response.text();
+      const $ = await cheerio.load(html);
+
+      //get the sub-divs of the div which contains the urls of each cities
+      const divs = $("div.field__items").children();
+
+      //push all the links into the UrlCities Array except the last 6 ones
+      //they are the same on each pages
+      divs.slice(0, divs.length - 6)
+      .map(id => UrlCities.push(divs[id].children[0].attribs.href));
+
+      return UrlCities; //return the UrlCities propreties of the object
     }
     catch(error) {
       console.log(error);
     }
-
   }
 
   return {
     getLink: (n) => { return Links[n]; },
-    getUrlCities = () => { return UrlCities; }
+    getUrlCities: () => { return UrlCities; },
+
+    loadUrlsCitiesFromLinks: (url) => {return loadUrlsCitiesFromLinks(url);}
   }
 }
+
+
+const cities = Cities();
+
+Promise.all([
+  cities.loadUrlsCitiesFromLinks(cities.getLink(0)),
+  cities.loadUrlsCitiesFromLinks(cities.getLink(1)),
+  cities.loadUrlsCitiesFromLinks(cities.getLink(2)),
+  cities.loadUrlsCitiesFromLinks(cities.getLink(3))
+]).then(() => {
+  cities.getUrlCities().map(url => console.log(url));
+});
